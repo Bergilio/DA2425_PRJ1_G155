f7801047381a4e3caa6ab5a8b1dba87a780a5862
Pedro Salgado <pedrosalgado7704@gmail.com>
2025-03-14 18:27:57 +0000

    Graph Creation Finished

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/pedro/CLionProjects/untitled
+# Install script for directory: /home/pedro/CLionProjects/DA2425_PRJ1_G155
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -57,6 +57,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "/home/pedro/CLionProjects/untitled/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "/home/pedro/CLionProjects/DA2425_PRJ1_G155/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
